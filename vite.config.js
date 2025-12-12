914019c563accc73a21c27d3fabfaccf849b5a0a
Joao Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2025-12-12 12:19:20 -0300

    chore: vue/inertia setup

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -17,4 +18,19 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        origin: 'http://localhost:5173',
+        cors: {
+            origin: 'http://localhost:8000',
+            credentials: true,
+        },
+    },
 });
